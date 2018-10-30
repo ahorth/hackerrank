99ea56613f8b3842c2b8fb1ded9360180b6562a9
Alexandre <alexhorth@me.com>
2018-10-29 21:05:47 -0400

    slight mod to solution to make it more compact

--- 9-jumping-on-the-clouds/jumping-on-the-clouds.hs
+++ 9-jumping-on-the-clouds/jumping-on-the-clouds.hs
@@ -2,8 +2,7 @@ solve :: Int -> [Int]-> Int
 
 solve count []   = count
 solve count [x]  = count + 1
-solve count (x1:x2:xs) |x1 == 1   = solve (count+1) xs 
-                       |x2 == 1   = solve (count+1 )(x2:xs)
+solve count (x1:x2:xs) |x2 == 1   = solve (count+2 )(tail xs)
                        |otherwise = solve (count+1) xs 
 
 main = interact $ show . solve 0 . map read . tail . tail . words
